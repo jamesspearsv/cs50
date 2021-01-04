07bd955e1b95beed31f137ea97db07147ed233a9
jamesspearsv <jamesspearsv@gmail.com>
2021-01-04 01:33:07 -0500

    .gitignore databasefiles and add pset7

---
+++ pset7/movies/13.sql
@@ -0,0 +1,6 @@
+SELECT DISTINCT(name) FROM stars
+JOIN people ON stars.person_id = people.id
+WHERE name!="Kevin Bacon" AND movie_id IN
+(SELECT movie_id FROM stars
+JOIN people ON stars.person_id = people.id
+WHERE name="Kevin Bacon" AND birth=1958);
